9e4b8c24afd6df1a7eb7a645a1c2fbb3ef10ea4e
David Silva <davidhenriquesi@live.com>
2015-06-10 15:12:09 -0300

    Helllllllllllllllllllllllllllll Yeah

--- SimSuas/app.js
+++ SimSuas/app.js
@@ -9,6 +9,7 @@ var http = require('http');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
+var home = require('./routes/home');
 
 var app = express();
 
@@ -26,6 +27,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/users', users);
+app.use('/home',home);
 
 
 
