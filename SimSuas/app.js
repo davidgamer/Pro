38950d272b6e2507ad1cfcaab1d7733e604b7070
David Silva <davidhenriquesi@live.com>
2015-06-15 14:41:15 -0300

    Radio Funcionando

--- SimSuas/app.js
+++ SimSuas/app.js
@@ -41,7 +41,7 @@ app.use('/sissuas', sisuas);
 //load('models').then('controllers').then('routes').into(app);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   var err = new Error('Not Found');
   err.status = 404;
   next(err);
@@ -52,7 +52,7 @@ app.use(function(req, res, next) {
 // development error handler
 // will print stacktrace
 if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
+  app.use(function (err, req, res, next) {
     res.status(err.status || 500);
     res.render('error', {
       message: err.message,
@@ -63,7 +63,7 @@ if (app.get('env') === 'development') {
 
 // production error handler
 // no stacktraces leaked to user
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   res.status(err.status || 500);
   res.render('error', {
     message: err.message,
