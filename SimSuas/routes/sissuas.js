e86e368ee6800bccca5210de14836ea17a09f4b3
David Silva <davidhenriquesi@live.com>
2015-06-16 14:35:43 -0300

    envai

--- SimSuas/routes/sissuas.js
+++ SimSuas/routes/sissuas.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('sissuas/home');
+  res.render('Sisuas/home');
 });
 
 module.exports = router;
